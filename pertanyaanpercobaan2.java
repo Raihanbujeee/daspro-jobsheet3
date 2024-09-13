e881b378ec25d5f95264dafa4a5d9f60c8fe40fd
Raihanbujeee <bujexixi@gmail.com>
2024-09-13 09:55:06 +0700

    pertanyaan2

--- pertanyaanpercobaan2.java
+++ pertanyaanpercobaan2.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-public class kafe24 {
+public class pertanyaanpercobaan2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         boolean keanggotaan;
-        int jmlKopi,jmlTeh,jmlRoti;
+        int jmlKopi,jmlTeh,jmlRoti,nominalInt;
         double hargaKopi = 12000.0,hargaTeh = 7000.0,hargaRoti = 20000.0, totalHarga,nominalbayar;
         float diskon = 10/100;
+        byte totalByte;
         System.out.print(" Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
         System.out.print("Masukkan jumlah pembelian kopi: ");
@@ -17,10 +18,12 @@ public class kafe24 {
         System.out.print("Masukkan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
         totalHarga = (jmlKopi*hargaKopi)+(jmlTeh*hargaTeh)+(jmlRoti*hargaRoti);
-        nominalbayar = totalHarga - (diskon *totalHarga);
-        System.err.println("Keanggotaan pelanggan " + keanggotaan);
+        totalByte = (byte) totalHarga;
+        nominalbayar = totalByte - (diskon *totalByte);
+        nominalInt= (int) nominalbayar;
+        System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println(" Item pembelian " +  jmlKopi+ " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti ");
-        System.out.println("Nominal bayar Rp " + nominalbayar);
+        System.out.println("Nominal bayar Rp " + nominalInt);
 
     }
 }
